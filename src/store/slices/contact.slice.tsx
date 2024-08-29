9e169cc8cfe2ab69b30d55c4934224e809bb5b57
MishraRahul7 <rahulmishra6396@gmail.com>
2024-08-29 15:36:58 +0530

    first commit

---
+++ src/store/slices/contact.slice.tsx
@@ -0,0 +1,18 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const contactSlice = createSlice({
+    name:"contact",
+    initialState:[],
+    reducers:{
+        addContact:(state,action)=>{
+
+        },
+        editContact:(state,action)=>{
+
+        },
+        deleteContact:(state,action)=>{
+
+        },
+    }
+})
+export default contactSlice.reducer;
